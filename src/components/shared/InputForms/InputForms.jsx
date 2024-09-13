2868e05e60c62d0b0793242395de2bd14ed5947e
Jero Arias <ariasmosquerajeronimo@gmail.com>
2024-09-13 01:54:36 -0500

    dada

--- src/components/shared/InputForms/InputForms.jsx
+++ src/components/shared/InputForms/InputForms.jsx
@@ -192,3 +192,82 @@ export const AsistenceBuscador = ({ value, onChange, possibleMatch, onSelectMatc
   );
 };
 export default AsistenceBuscador;
+
+// CustomInput Component
+export const CustomInput = ({ label, type = "text", inputProps, inputRef }) => {
+  const [used, setUsed] = useState(false);
+  const [focused, setFocused] = useState(false);
+
+  const validateActive = (focused) => {
+    if (!inputRef.current) return;
+    const { value } = inputRef.current;
+    setUsed(value.length !== 0 || focused);
+  };
+
+  const labelClasses = () => {
+    const actived = used
+      ? "text-[20px] left-5 -translate-y-8 bg-white px-3"
+      : "text-[25px] left-5";
+    return `absolute text-[#692FDB] transition-[transform_font-size_padding] duration-[250ms] ${actived}`;
+  };
+  return (
+    <span className="relative flex items-center w-full">
+      <label className={labelClasses()}>{label}</label>
+      <input
+        ref={inputRef}
+        type={type}
+        className="w-full h-[4rem] border-[4px] rounded-xl outline-[#381975] outline-offset-2 border-[#692FDB] px-4 placeholder:text-[#692FDB] text-[25px]  "
+        onFocus={() => {
+          setFocused(true);
+          validateActive(true);
+        }}
+        onBlur={() => {
+          setFocused(false);
+          validateActive(false);
+        }}
+        {...inputProps}
+      />
+    </span>
+  );
+};
+
+// CustomTextArea Component
+export const CustomTextArea = ({ label, type = "text", inputProps, inputRef }) => {
+  const [used, setUsed] = useState(false);
+  const [focused, setFocused] = useState(false);
+
+  const validateActive = (focused) => {
+    if (!inputRef.current) return;
+    const { value } = inputRef.current;
+    setUsed(value.length !== 0 || focused);
+  };
+
+  const labelClasses = () => {
+    const actived = used
+      ? "text-[20px] left-5 -translate-y-8 bg-white px-3"
+      : "text-[25px] left-5";
+    return `absolute text-[#692FDB] transition-[transform_font-size_padding] duration-[250ms] ${actived}`;
+  };
+  return (
+    <span className="relative flex items-center w-full">
+      <label className={labelClasses()}>{label}</label>
+      <input
+        ref={inputRef}
+        placeholder={label}
+        className="w-full py-1 border-[4px] rounded-xl outline-[#381975] outline-offset-2 border-[#692FDB] px-4 placeholder:text-[#692FDB] text-[25px]"
+        onFocus={() => {
+          setFocused(true);
+          validateActive(true);
+        }}
+        onBlur={() => {
+          setFocused(false);
+          validateActive(false);
+        }}
+        {...inputProps}
+      />
+    </span>
+  );
+  ref = { inputRef }
+  placeholder = { label }
+  className = "w-full py-1 border-[4px] rounded-xl outline-[#381975] outline-offset-2 border-[#692FDB] px-4 placeholder:text-[#692FDB] text-[25px]"
+};
--- src/components/shared/inventorySelector/inventorySelector.jsx
+++ src/components/shared/inventorySelector/inventorySelector.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { CiSearch } from "react-icons/ci";
 import { LiaExchangeAltSolid } from "react-icons/lia";
 
+import { CreateBien } from "../../Modals/ModalAggregate/CreateBien";
+import { StateContext } from '../../Context/Context'
+import { CreateProducts } from "../../Modals/ModalAggregate/CreateProducts";
+
+
 function nextInventory(code) {
 switch (numer) {
   case value:
@@ -21,18 +26,46 @@ export const InventorySelectorBienes = () => {
         <h1 className="text-[3rem]">Bienes</h1>
         <LiaExchangeAltSolid className="text-[2.5rem] cursor-pointer" />
       </div>
+
+
+      <div className=" flex justify-center items-center bg-[#692FDB] size-[4rem] rounded-lg fixed right-[3rem] bottom-[3rem]">
+        <FaPlus className=" cursor-pointer text-[2.5rem] text-white" onClick={() => 
+          
+            setmodalCreateBienes(true)          
+        }
+           />
+      </div>
+      <CreateBien
+          visibility={modalCreateBienes}
+          closeIcon={() => setmodalCreateBienes(false)}
+        />
     </div>
   );
 };
 
-export const InventorySelectorProducts = () => {
+export const InventorySelectorProductos = ({ onNext, onSearch }) => {
+  const { modalCreateProducts, setModalCreateProducts } = useContext(StateContext);
+
   return (
     <div className="flex justify-center">
-      <div className="flex justify-between w-[50%] h-[5rem] items-center text-white bg-gradient-to-r from-[#FE7A36]  to-[#FF9F2E] px-9 py-1 rounded-[10px] shadow-md ">
-        <CiSearch className="text-[2.5rem] cursor-pointer" />
-        <h1 className="text-[3rem]">Productos</h1>
-        <LiaExchangeAltSolid className="text-[2.5rem] cursor-pointer" />
+      <div className="flex justify-between w-[50%] h-[5rem] items-center text-white bg-gradient-to-r from-[#FE7A36] to-[#FF9F2E] px-9 py-1 rounded-[10px] shadow-md">
+        <CiSearch className="text-[2.5rem] cursor-pointer" onClick={onSearch} />
+        <h1 className="select-none text-[3rem]">Productos</h1>
+        <LiaExchangeAltSolid
+          onClick={onNext}
+          className="text-[2.5rem] cursor-pointer"
+        />
       </div>
+      <div className="cursor-pointer flex justify-center items-center bg-[#FE7A36] w-[4rem] h-[4rem] rounded-lg fixed right-[3rem] bottom-[3rem]">
+        <FaPlus
+          className="cursor-pointer text-[2.5rem] text-white"
+          onClick={() => setModalCreateProducts(true)}
+        />
+      </div>
+      <CreateProducts
+        visibility={modalCreateProducts}
+        closeIcon={() => setModalCreateProducts(false)}
+      />
     </div>
   );
 };
